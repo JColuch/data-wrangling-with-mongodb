ec4ba705aa1e03f6c062c49bad9bed65f7921428
JoelColucci <JoelColucci@Gmail.com>
2015-04-09 21:31:55 -0400

    added pset answers and lesson 6

---
+++ psets/pset-2-3.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Let's assume that you combined the code from the previous 2 exercises
+# with code from the lesson on how to build requests, and downloaded all the data locally.
+# The files are in a directory "data", named after the carrier and airport:
+# "{}-{}.html".format(carrier, airport), for example "FL-ATL.html".
+# The table with flight info has a table class="dataTDRight".
+# There are couple of helper functions to deal with the data files.
+# Please do not change them for grading purposes.
+# All your changes should be in the 'process_file' function
+# This is example of the datastructure you should return
+# Each item in the list should be a dictionary containing all the relevant data
+# Note - year, month, and the flight data should be integers
+# You should skip the rows that contain the TOTAL data for a year
+# data = [{"courier": "FL",
+#         "airport": "ATL",
+#         "year": 2012,
+#         "month": 12,
+#         "flights": {"domestic": 100,
+#                     "international": 100}
+#         },
+#         {"courier": "..."}
+# ]
+from bs4 import BeautifulSoup
+from zipfile import ZipFile
+import os
+
+datadir = "data"
+
+
+def open_zip(datadir):
+    with ZipFile('{0}.zip'.format(datadir), 'r') as myzip:
+        myzip.extractall()
+
+
+def process_all(datadir):
+    files = os.listdir(datadir)
+    return files
+
+
+def process_file(f):
+    # This is example of the datastructure you should return
+    # Each item in the list should be a dictionary containing all the relevant data
+    # Note - year, month, and the flight data should be integers
+    # You should skip the rows that contain the TOTAL data for a year
+    # data = [{"courier": "FL",
+    #         "airport": "ATL",
+    #         "year": 2012,
+    #         "month": 12,
+    #         "flights": {"domestic": 100,
+    #                     "international": 100}
+    #         },
+    #         {"courier": "..."}
+    # ]
+    data = []
+    info = {}
+    info["courier"], info["airport"] = f[:6].split("-")
+    
+    with open("{}/{}".format(datadir, f), "r") as html:
+
+        soup = BeautifulSoup(html)
+        table_elm = soup.find("table", {"class": "dataTDRight"})
+    
+        table_rows = table_elm.find_all("tr")
+
+        #Set flag value for first row
+        is_first_row = True
+        #iterate through each row
+        for row in table_rows:
+            #Skip first headers row
+            if is_first_row:
+                is_first_row = False
+                continue
+            
+            row_data = row.find_all("td")
+
+            #Skip any row that is a "total" row
+            month = row_data[1].text
+            if month == "TOTAL":
+                continue
+            month = int(month)
+            courier = info["courier"]
+            airport = info["airport"]
+            year = int(row_data[0].text)
+            domestic = int(row_data[2].text.replace(",",""))
+            international = int(row_data[3].text.replace(",",""))
+
+            #Assemble data obj
+            row_obj = {
+                "courier": courier,
+                "airport": airport,
+                "year": year,
+                "month": month,
+                "flights": {"domestic": domestic,
+                         "international": international}
+            }
+
+            data.append(row_obj)
+
+    return data
+
+
+def test():
+    print "Running a simple test..."
+    open_zip(datadir)
+    files = process_all(datadir)
+    data = []
+    for f in files:
+        data += process_file(f)
+    assert len(data) == 399
+    for entry in data[:3]:
+        assert type(entry["year"]) == int
+        assert type(entry["month"]) == int
+        assert type(entry["flights"]["domestic"]) == int
+        assert len(entry["airport"]) == 3
+        assert len(entry["courier"]) == 2
+    assert data[-1]["airport"] == "ATL"
+    assert data[-1]["flights"] == {'international': 108289, 'domestic': 701425}
+    
+    print "... success!"
+
+if __name__ == "__main__":
+    test()
---
+++ psets/pset-3-3.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+In this problem set you work with cities infobox data, audit it, come up with a cleaning idea and then clean it up.
+
+Since in the previous quiz you made a decision on which value to keep for the "areaLand" field,
+you now know what has to be done.
+
+Finish the function fix_area(). It will receive a string as an input, and it has to return a float
+representing the value of the area or None.
+You have to change the function fix_area. You can use extra functions if you like, but changes to process_file
+will not be taken into account.
+The rest of the code is just an example on how this function can be used.
+"""
+import codecs
+import csv
+import json
+import pprint
+
+CITIES = 'cities.csv'
+
+
+def is_int(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+def is_float(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+
+def fix_area(area):
+    # Based on our audit research we know our dataset has 3 types for area
+    # Now we can target those 3 types
+    #Handle str
+    if is_float(area):
+        area = float(area)
+    #Handle list
+    elif area.strip().startswith("{"):
+        #Strip brackets
+        area = area.replace("{","")
+        area = area.replace("}", "")
+
+        #Split string
+        val_one, val_two = area.split("|")
+
+        if len(val_one) > len(val_two):
+            area = float(val_one)
+        else:
+            area = float(val_two)
+    #Handle NoneType
+    else:
+        area = None
+
+    return area
+
+
+
+def process_file(filename):
+    # CHANGES TO THIS FUNCTION WILL BE IGNORED WHEN YOU SUBMIT THE EXERCISE
+    data = []
+
+    with open(filename, "r") as f:
+        reader = csv.DictReader(f)
+
+        #skipping the extra metadata
+        for i in range(3):
+            l = reader.next()
+
+        # processing file
+        for line in reader:
+            # calling your function to fix the area value
+            if "areaLand" in line:
+                line["areaLand"] = fix_area(line["areaLand"])
+            data.append(line)
+
+    return data
+
+
+def test():
+    data = process_file(CITIES)
+
+    print "Printing three example results:"
+    for n in range(5,8):
+        pprint.pprint(data[n]["areaLand"])
+
+    assert data[8]["areaLand"] == 55166700.0
+    assert data[3]["areaLand"] == None
+
+
+if __name__ == "__main__":
+    test()
